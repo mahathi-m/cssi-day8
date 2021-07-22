8bc869e6de43f7e75208f2a31ded4ec413b50215
Mahathi Mangipudi <mahathimangipudi@gmail.com>
2021-07-22 18:35:29 +0000

    day 9 write message

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,3 +1,4 @@
+/*
 const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
@@ -28,11 +29,34 @@ const findMessages = (myPass) => {
     });
 } 
 
-const renderMessage = 
+const renderMessage = () => {
+
+}
+*/
+
+const display = (messageObject) => {
+    console.log(messageObject);
+    document.querySelector("#message").innerHTML = messageObject.message;
+
+}
 
 
-document.querySelector.addEventListener("click", getMessages);
+document.querySelector("#viewMsg").addEventListener("click", (e) => {
+    const userPasscodeGuess = document.querySelector("#passcode").value;
+    const messagesRef = firebase.database().ref();
+    messagesRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        for(let key in data) {
+            if(data[key].passcode == userPasscodeGuess) {
+            display(data[key]);
+            }
+        }
+    });
+});
 
-const passcode = document.querySelector("#viewMsg").nodeValue;
-findMessages(passcode);
+
+
+
+//const passcode = document.querySelector("#viewMsg").nodeValue;
+//findMessages(passcode);
 
